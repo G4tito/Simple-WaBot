9184b166e94a580b36f734975028888b524f4aa9
Gatito <130865008+G4tito@users.noreply.github.com>
2024-07-11 09:35:51 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,19 +1,43 @@
-const prefix = '/';
+const { plugins } = require('./lib/plugins.js');
+
+const prefixList = ['/', '!', '#'];
 
 const handler = async (msg, sock) => {
     try {
-        if (!msg.text.startsWith(prefix))
-            return;
+        const prefixRegex = new RegExp('^[' + prefixList.map(c => '\\' + c).join('') + ']');
+        const match = msg.text.match(prefixRegex);
+        const prefix = match ? match[0] : '';
+        
+        const trimText = msg.text.slice(prefix.length).trim();
+        const command = trimText.split(/\s+/)[0].toLowerCase();
+        const text = trimText.replace(command, '').trim();
+        const args = text.split(/\s+/);
+        
+        const isCmd = command.length === 0 ? false : !!prefix;
 
-        const args = msg.text.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-
-        if (command === 'alive') {
-            msg.reply('Hello there!');
+        const { cmd: cmds } = plugins;
+        if (isCmd) {
+            const cmd = cmds.map(c => Object.values(c)[0]).find((v) => v.command.find((x) => x.toLowerCase() === command)) || '';
+            if (cmd) {
+                cmd.start({
+                    msg,
+                    sock,
+                    
+                    text,
+                    args,
+                    
+                    prefix,
+                    command,
+                    
+                    plugins: {
+                        cmds
+                    }
+                });
+            }
         }
     } catch (e) {
         console.error(e);
     }
-}
+};
 
 module.exports = handler;
