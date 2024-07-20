0eb6aac23ba1477b4ba6e68caba2207cb993c3b8
Gatito <130865008+G4tito@users.noreply.github.com>
2024-07-20 02:11:27 -0500

    Add files via upload

--- plugins/download/ytmp3.js
+++ plugins/download/ytmp3.js
@@ -1,14 +1,16 @@
 const { formatSize } = require('../../lib/func.js');
+const y2mate = require('../../lib/scraper/y2mate.js');
 const youtube = require('../../lib/scraper/youtube.js');
+const ufs = require('../../lib/ufs.js');
 
-let isLimit = 15; // 15 MB
+const isLimit = 15; // 15 MB
 
 exports.cmd = {
     name: ['ytmp3'],
     command: ['ytmp3'],
     category: ['download'],
     detail: {
-        desc: 'Descarga de audios de YouTube.',
+        desc: 'Descarga el audio del video de YouTube.',
         use: '@url=[yt]'
     },
     setting: {
@@ -20,30 +22,31 @@ exports.cmd = {
         }
         
         if (!isYouTubeUrl(text)) {
-            return msg.reply('Ingresa el enlace válido del video de *YouTube* que deseas descargar.');
+            return msg.reply('Ingresa un enlace válido del video de *YouTube* que deseas descargar.');
         }
 
         await msg.react('🕓');
-        let { status, result } = await youtube.download(text, 'audio');
         
-        if (!status) {
-            await msg.react('✖');
-            return msg.reply('📛 | Hubo un error al obtener el resultado del audio.');
-        }
+            let { status, result } = await y2mate.download(text, 'audio') || await youtube.download(text, 'audio');
+            if (!status) {
+                await msg.react('✖');
+                return msg.reply('📛 | Hubo un error al obtener el resultado del audio.');
+            }
 
-        let size = await formatSize(result.size);
-        
-        if (Number(size.split(' MB')[0]) >= isLimit || Number(size.split(' GB')[0]) >= 0) {
-            await msg.react('✖');
-            return msg.reply(`El audio pesa ${size}, excede el límite máximo de descarga que es de ${isLimit} MB.`);
-        }
+            const audio = result.audio?.find(v => v.quality === '128') || { url: result.buffer };
+            const size = await formatSize(await ufs(audio.url) || result.size);
+            
+            if (Number(size.split(' MB')[0]) >= isLimit || Number(size.split(' GB')[0]) >= 0) {
+                await msg.react('✖');
+                return msg.reply(`El audio pesa ${size}, excede el límite máximo de descarga que es de ${isLimit} MB.`);
+            }
 
-        await msg.reply(null, { audio: result.buffer, fileName: result.title + '.mp3', mimetype: 'audio/mpeg' });
-        await msg.react('✅');
+            await msg.reply(null, { audio: audio.url, fileName: result.title + '.mp3', mimetype: 'audio/mpeg' });
+            await msg.react('✅');
     }
 };
 
 function isYouTubeUrl(url) {
-    let regex = /^(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)\/?(?:\?.*v=|\/)?)([a-zA-Z0-9_-]+)/;
+    const regex = /^(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)\/?(?:\?.*v=|\/)?)([a-zA-Z0-9_-]+)/;
     return regex.test(url);
 }
--- plugins/download/ytmp4.js
+++ plugins/download/ytmp4.js
@@ -1,14 +1,16 @@
 const { formatSize } = require('../../lib/func.js');
+const y2mate = require('../../lib/scraper/y2mate.js');
 const youtube = require('../../lib/scraper/youtube.js');
+const ufs = require('../../lib/ufs.js');
 
-let isLimit = 70; // 70 MB
+const isLimit = 70; // 70 MB
 
 exports.cmd = {
     name: ['ytmp4'],
     command: ['ytmp4'],
     category: ['download'],
     detail: {
-        desc: 'Descarga de videos de YouTube.',
+        desc: 'Descarga el video de YouTube.',
         use: '@url=[yt]'
     },
     setting: {
@@ -20,30 +22,31 @@ exports.cmd = {
         }
         
         if (!isYouTubeUrl(text)) {
-            return msg.reply('Ingresa el enlace válido del video de *YouTube* que deseas descargar.');
+            return msg.reply('Ingresa un enlace válido del video de *YouTube* que deseas descargar.');
         }
 
         await msg.react('🕓');
-        let { status, result } = await youtube.download(text, 'video');
         
-        if (!status) {
-            await msg.react('✖');
-            return msg.reply('📛 | Hubo un error al obtener el resultado del vídeo.');
-        }
+            let { status, result } = await y2mate.download(text, 'video') || await youtube.download(text, 'video');
+            if (!status) {
+                await msg.react('✖');
+                return msg.reply('📛 | Hubo un error al obtener el resultado del vídeo.');
+            }
 
-        let size = await formatSize(result.size);
-        
-        if (Number(size.split(' MB')[0]) >= isLimit || Number(size.split(' GB')[0]) >= 0) {
-            await msg.react('✖');
-            return msg.reply(`El video pesa ${size}, excede el límite máximo de descarga que es de ${isLimit} MB.`);
-        }
+            const video = result.video?.find(v => v.quality === '360') || { url: result.buffer };
+            const size = await formatSize(await ufs(video.url) || result.size);
+            
+            if (Number(size.split(' MB')[0]) >= isLimit || Number(size.split(' GB')[0]) >= 0) {
+                await msg.react('✖');
+                return msg.reply(`El video pesa ${size}, excede el límite máximo de descarga que es de ${isLimit} MB.`);
+            }
 
-        await msg.reply(result.title, { video: result.buffer });
-        await msg.react('✅');
+            await msg.reply(result.title, { video: video.url });
+            await msg.react('✅');
     }
 };
 
 function isYouTubeUrl(url) {
-    let regex = /^(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)\/?(?:\?.*v=|\/)?)([a-zA-Z0-9_-]+)/;
+    const regex = /^(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)\/?(?:\?.*v=|\/)?)([a-zA-Z0-9_-]+)/;
     return regex.test(url);
 }
