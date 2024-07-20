875242f427323090ccdd0d5a288ea3ccb9337435
Gatito <130865008+G4tito@users.noreply.github.com>
2024-07-20 11:14:21 -0500

    Create kick.js

---
+++ plugins/moderation/kick.js
@@ -0,0 +1,26 @@
+exports.cmd = {
+    name: ['kick'],
+    command: ['kick'],
+    category: ['moderation'],
+    detail: {
+        desc: 'Elimina a un usuario del grupo.',
+        use: '@user'
+    },
+    setting: {
+        isGroup: true,
+        isAdmin: true,
+        isBotAdmin: true
+    },
+    async start({ msg, participants }) {
+        let who = msg.quoted ? msg.quoted.sender : msg.mentions[0];
+        if (!who) {
+            return msg.reply('🔖 | *Menciona* o *responde* el *mensaje* del *usuario* que deseas *eliminar*.');
+        }
+        let member = participants.find(u => u.id === who);
+        if (!member) {
+            return msg.reply('El usuario no está en el grupo.');
+        }
+        sock.groupParticipantsUpdate(m.chat, [who], 'remove');
+        await msg.reply(`✔ | El usuario @${who.split('@')[0]} ha sido eliminado con éxito.`);
+    }
+};
