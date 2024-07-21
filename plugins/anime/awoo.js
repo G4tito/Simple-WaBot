c5cb278c0c072a0ca2214efec9c18de69398c7e0
Gatito <130865008+G4tito@users.noreply.github.com>
2024-07-20 23:31:24 -0500

    Add files via upload

---
+++ plugins/anime/awoo.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+exports.cmd = {
+    name: ['awoo'],
+    command: ['awoo'],
+    category: ['anime'],
+    detail: {
+        desc: 'Envia una imagen aleatoria de una chica lobo.'
+    },
+    setting: {
+        error_react: true
+    },
+    async start({ msg }) {
+        await msg.react('🕓');
+        let res = await axios.get('https://api.waifu.pics/sfw/awoo');
+        await msg.reply('Random awoo image.', { image: res.data.url });
+        await msg.react('✅');
+    }
+};
