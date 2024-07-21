622332df8a27aabd74a9d58c39092dd1c8af93fd
Gatito <130865008+G4tito@users.noreply.github.com>
2024-07-20 19:11:33 -0500

    Add files via upload

--- plugins/script/update.js
+++ plugins/script/update.js
@@ -27,13 +27,13 @@ exports.cmd = {
         [created, deleted, files].forEach((list, index) => {
             const titles = ['created', 'deleted', 'files'];
             if (list.length > 0) {
-                teks += ` • ${titles[index].replace(/^\w/, c => c.toUpperCase())}:\n${list.map(item => `- ${item}\n`).join('\n')}\n`;
+                teks += ` • ${titles[index].replace(/^\w/, c => c.toUpperCase())}:\n${list.map(item => `- ${item}`).join('\n')}\n\n`;
             }
         });
 
         ['deletions', 'insertions'].forEach(key => {
             if (Object.keys(result[key]).length > 0) {
-                teks += ` • ${key.replace(/^\w/, c => c.toUpperCase())}:\n${Object.entries(result[key]).map(([file, count]) => `- ${file} | ${count}`).join('\n')}\n`;
+                teks += ` • ${key.replace(/^\w/, c => c.toUpperCase())}:\n${Object.entries(result[key]).map(([file, count]) => `- ${file} | ${count}`).join('\n')}\n\n`;
             }
         });
 
