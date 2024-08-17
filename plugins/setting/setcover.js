0ddbe8b7bf227223150cb32b1079741995accc60
Gatito <130865008+G4tito@users.noreply.github.com>
2024-08-16 19:30:10 -0500

    Add files via upload

---
+++ plugins/setting/setcover.js
@@ -0,0 +1,27 @@
+const { uploadToTelegraph } = require('../../lib/sticker.js');
+
+exports.cmd = {
+    name: ['setcover'],
+    command: ['setcover', 'setthumb'],
+    category: ['setting'],
+    detail: {
+        desc: 'Cambia la miniatura del menú del bot.',
+        use: 'img'
+    },
+    async start({ msg, sock, args, db }) {
+        let setting = db.settings.get(sock.user.jid);
+
+        if (args[0] === '--default') {
+            setting.cover = '';
+            return msg.reply('*🚩 La miniatura del menú se ha cambiado a predeterminado con éxito.*');
+        }
+
+        let q = msg.quoted ? msg.quoted : msg;
+        if (!/image/.test(q.type)) return msg.reply('*🚩 Responde a una imagen para cambiar la miniatura del menú.*');
+
+        let buffer = await q.download();
+        setting.cover = await uploadToTelegraph(buffer);
+        await db.save();
+        await msg.reply('*🚩 La miniatura del menú se ha cambiado con éxito.*');
+    }
+};
